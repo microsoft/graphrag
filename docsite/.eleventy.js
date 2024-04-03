e4b55fd9ad06de3227572c310b1302b574a31159
Alonso Guevara <alonsog@microsoft.com>
2024-04-03 16:25:33 -0600

    Add copy to clipboard button

--- docsite/.eleventy.js
+++ docsite/.eleventy.js
@@ -1,11 +1,20 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const codeClipboard = require("eleventy-plugin-code-clipboard");
 const pluginMermaid = require("@kevingimbel/eleventy-plugin-mermaid");
+const markdownIt = require('markdown-it');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(codeClipboard);
   eleventyConfig.addPlugin(pluginMermaid);
   eleventyConfig.addPassthroughCopy("data");
   eleventyConfig.addPassthroughCopy("img");
   // Verbs and Workflows are auto-generated
   eleventyConfig.setUseGitIgnore(false);
+
+  const markdownLibrary = markdownIt({
+    html: true
+  }).use(codeClipboard.markdownItCopyButton);
+  
+  eleventyConfig.setLibrary("md", markdownLibrary);
 };
