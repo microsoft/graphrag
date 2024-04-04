c308011ed7ce8a5fbef54d2a28b7b6b1aeb11b39
Chris Trevino <chtrevin@microsoft.com>
2024-04-04 08:55:24 -0700

    update docsite pathprefix

--- docsite/.eleventy.js
+++ docsite/.eleventy.js
@@ -1,3 +1,4 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const codeClipboard = require("eleventy-plugin-code-clipboard");
 const pluginMermaid = require("@kevingimbel/eleventy-plugin-mermaid");
@@ -7,6 +8,9 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(codeClipboard);
   eleventyConfig.addPlugin(pluginMermaid);
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
+    baseHref: process.env.DOCSITE_BASE_URL || ""
+  });
   eleventyConfig.addPassthroughCopy("data");
   eleventyConfig.addPassthroughCopy("img");
   // Verbs and Workflows are auto-generated
