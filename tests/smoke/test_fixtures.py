1c17ad2ae033ff30eb6ea9d08149ddcefca7ef28
Chris Trevino <chtrevin@microsoft.com>
2024-04-19 16:46:56 -0700

    add blob-storage fallback

--- tests/smoke/test_fixtures.py
+++ tests/smoke/test_fixtures.py
@@ -244,6 +244,9 @@ class TestIndexer:
         os.environ,
         {
             **os.environ,
+            "BLOB_STORAGE_CONNECTION_STRING": os.getenv(
+                "BLOB_STORAGE_CONNECTION_STRING", WELL_KNOWN_AZURITE_CONNECTION_STRING
+            ),
             "LOCAL_BLOB_STORAGE_CONNECTION_STRING": WELL_KNOWN_AZURITE_CONNECTION_STRING,
             "GRAPHRAG_CHUNK_SIZE": "1200",
             "GRAPHRAG_CHUNK_OVERLAP": "0",
