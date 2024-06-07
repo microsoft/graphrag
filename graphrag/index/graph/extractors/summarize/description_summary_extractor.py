977702e6426901e1bdceeb197c72ce8c58b4eaaa
Alonso Guevara <alonsog@microsoft.com>
2024-06-06 18:23:23 -0600

    Small refactoring

--- graphrag/index/graph/extractors/summarize/description_summary_extractor.py
+++ graphrag/index/graph/extractors/summarize/description_summary_extractor.py
@@ -12,6 +12,11 @@ from graphrag.llm import CompletionLLM
 
 from .prompts import SUMMARIZE_PROMPT
 
+# Max token size for input prompts
+DEFAULT_MAX_INPUT_TOKENS = 4000
+# Max token count for LLM answers
+DEFAULT_MAX_SUMMARY_LENGTH = 500
+
 
 @dataclass
 class SummarizationResult:
@@ -50,8 +55,8 @@ class SummarizeExtractor:
 
         self._summarization_prompt = summarization_prompt or SUMMARIZE_PROMPT
         self._on_error = on_error or (lambda _e, _s, _d: None)
-        self._max_summary_length = max_summary_length or 500
-        self._max_input_tokens = max_input_tokens or 4000
+        self._max_summary_length = max_summary_length or DEFAULT_MAX_SUMMARY_LENGTH
+        self._max_input_tokens = max_input_tokens or DEFAULT_MAX_INPUT_TOKENS
 
     async def __call__(
         self,
@@ -65,50 +70,66 @@ class SummarizeExtractor:
         if len(descriptions) == 1:
             result = descriptions[0]
         else:
-            sorted_items = sorted(items) if isinstance(items, list) else items
-
-            # Safety check, should always be a list
-            if not isinstance(descriptions, list):
-                descriptions = [descriptions]
-
-            # Iterate over descriptions, adding all until the max input tokens is reached
-            usable_tokens = self._max_input_tokens - num_tokens_from_string(
-                self._summarization_prompt
-            )
-            descriptions_collected = []
-
-            for i, description in enumerate(descriptions):
-                usable_tokens -= num_tokens_from_string(description)
-                descriptions_collected.append(description)
-
-                # If buffer is full, or all descriptions have been added, summarize
-                if (usable_tokens < 0 and len(descriptions_collected) > 1) or (
-                    i == len(descriptions) - 1
-                ):
-                    response = await self._llm(
-                        self._summarization_prompt,
-                        name="summarize",
-                        variables={
-                            self._entity_name_key: json.dumps(sorted_items),
-                            self._input_descriptions_key: json.dumps(
-                                sorted(descriptions)
-                            ),
-                        },
-                        model_parameters={"max_tokens": self._max_summary_length},
-                    )
-                    # Calculate result (final or interim)
-                    result = str(response.output)
-
-                    # If we go for another loop, reset values to new
-                    if i != len(descriptions) - 1:
-                        descriptions_collected = [result]
-                        usable_tokens = (
-                            self._max_input_tokens
-                            - num_tokens_from_string(self._summarization_prompt)
-                            - num_tokens_from_string(result)
-                        )
+            result = await self._summarize_descriptions(items, descriptions)
 
         return SummarizationResult(
             items=items,
             description=result or "",
         )
+
+    async def _summarize_descriptions(
+        self, items: str | tuple[str, str], descriptions: list[str]
+    ) -> str:
+        """Summarize descriptions into a single description."""
+        sorted_items = sorted(items) if isinstance(items, list) else items
+
+        # Safety check, should always be a list
+        if not isinstance(descriptions, list):
+            descriptions = [descriptions]
+
+            # Iterate over descriptions, adding all until the max input tokens is reached
+        usable_tokens = self._max_input_tokens - num_tokens_from_string(
+            self._summarization_prompt
+        )
+        descriptions_collected = []
+        result = ""
+
+        for i, description in enumerate(descriptions):
+            usable_tokens -= num_tokens_from_string(description)
+            descriptions_collected.append(description)
+
+            # If buffer is full, or all descriptions have been added, summarize
+            if (usable_tokens < 0 and len(descriptions_collected) > 1) or (
+                i == len(descriptions) - 1
+            ):
+                # Calculate result (final or partial)
+                result = await self._summarize_descriptions_with_llm(
+                    sorted_items, descriptions
+                )
+
+                # If we go for another loop, reset values to new
+                if i != len(descriptions) - 1:
+                    descriptions_collected = [result]
+                    usable_tokens = (
+                        self._max_input_tokens
+                        - num_tokens_from_string(self._summarization_prompt)
+                        - num_tokens_from_string(result)
+                    )
+
+        return result
+
+    async def _summarize_descriptions_with_llm(
+        self, items: str | tuple[str, str] | list[str], descriptions: list[str]
+    ):
+        """Summarize descriptions using the LLM."""
+        response = await self._llm(
+            self._summarization_prompt,
+            name="summarize",
+            variables={
+                self._entity_name_key: json.dumps(items),
+                self._input_descriptions_key: json.dumps(sorted(descriptions)),
+            },
+            model_parameters={"max_tokens": self._max_summary_length},
+        )
+        # Calculate result
+        return str(response.output)
