51d9c4f203b7bfcbf8159cbc89a88b7ca870619e
Joshua Bradley <joshbradley@microsoft.com>
2024-07-21 05:50:01 +0000

    typo fix

--- tests/smoke/test_fixtures.py
+++ tests/smoke/test_fixtures.py
@@ -108,7 +108,7 @@ async def prepare_azurite_data(input_path: str, azure: dict) -> Callable[[], Non
     data_files = txt_files + csv_files
     for data_file in data_files:
         with data_file.open("rb") as f:
-            text = f.read().decode(encoding="utf8", errors="strict")
+            text = f.read().decode(encoding="utf-8", errors="strict")
         file_path = (
             str(Path(input_base_dir) / data_file.name)
             if input_base_dir
