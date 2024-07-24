2ddee65c29b2340664985c8d9a2678b41c2d1616
Josh Bradley <joshbradley@microsoft.com>
2024-07-24 13:28:22 -0400

    Read/write files as binary utf-8 (#639)

--- graphrag/config/models/entity_extraction_config.py
+++ graphrag/config/models/entity_extraction_config.py
@@ -38,7 +38,9 @@ class EntityExtractionConfig(LLMConfig):
             "type": ExtractEntityStrategyType.graph_intelligence,
             "llm": self.llm.model_dump(),
             **self.parallelization.model_dump(),
-            "extraction_prompt": (Path(root_dir) / self.prompt).read_text()
+            "extraction_prompt": (Path(root_dir) / self.prompt)
+            .read_bytes()
+            .decode(encoding="utf-8")
             if self.prompt
             else None,
             "max_gleanings": self.max_gleanings,
--- graphrag/prompt_tune/generator/entity_extraction_prompt.py
+++ graphrag/prompt_tune/generator/entity_extraction_prompt.py
@@ -99,7 +99,7 @@ def create_entity_extraction_prompt(
 
         output_path = output_path / ENTITY_EXTRACTION_FILENAME
         # Write file to output path
-        with output_path.open("w") as file:
-            file.write(prompt)
+        with output_path.open("wb") as file:
+            file.write(prompt.encode(encoding="utf-8", errors="strict"))
 
     return prompt
