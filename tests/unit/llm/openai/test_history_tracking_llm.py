eda5d507aaa94e83533fadcd5001ec0521ac34f8
Chris Trevino <chtrevin@microsoft.com>
2024-07-25 14:51:02 -0700

    unit test update

--- tests/unit/llm/openai/test_history_tracking_llm.py
+++ tests/unit/llm/openai/test_history_tracking_llm.py
@@ -1,18 +1,22 @@
+from typing import cast
 from graphrag.llm.openai.openai_history_tracking_llm import OpenAIHistoryTrackingLLM
-from graphrag.llm.mock.mock_completion_llm import MockCompletionLLM
+from graphrag.llm import CompletionLLM, LLMOutput
 
 async def test_history_tracking_llm() -> None:
-  delegate = MockCompletionLLM(["response 1", "response 2"])
+  async def mock_responder(input: str, **kwargs: dict) -> str:
+    return LLMOutput(output=f"response to [{input}]")
+  
+  delegate = cast(CompletionLLM, mock_responder)
   llm = OpenAIHistoryTrackingLLM(delegate)
 
   response = await llm("input 1")
   assert len(response.history) == 2
   assert response.history[0] == {"role": "user", "content": "input 1"}
-  assert response.history[1] == {"role": "system", "content": "response 1"}
+  assert response.history[1] == {"role": "system", "content": "response to [input 1]"}
 
   response = await llm("input 2", history=response.history)
   assert len(response.history) == 4
   assert response.history[0] == {"role": "user", "content": "input 1"}
-  assert response.history[1] == {"role": "system", "content": "response 1"}
+  assert response.history[1] == {"role": "system", "content": "response to [input 1]"}
   assert response.history[2] == {"role": "user", "content": "input 2"}
-  assert response.history[3] == {"role": "system", "content": "response 2"}
+  assert response.history[3] == {"role": "system", "content": "response to [input 2]"}
