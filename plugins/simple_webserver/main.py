05264be686cd45d360ff57b125f9358953ce07c3
jarlor <zjl58960902@outlook.com>
2024-08-01 15:03:59 +0800

    refactor:adjust poe task for webserver

--- plugins/simple_webserver/main.py
+++ plugins/simple_webserver/main.py
@@ -34,4 +34,4 @@ def run(item: GraphRAGItem):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, workers=4)
