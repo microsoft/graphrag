caae80e53a1db093d0bc081d361f3b519c0aa08b
Josh Bradley <joshbradley@microsoft.com>
2024-08-06 02:43:29 -0400

    initial setup commit

---
+++ graphrag/prompt_tune/api.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024 Microsoft Corporation.
+# Licensed under the MIT License
+
+"""
+Prompt Tuning API.
+
+This API provides access to the prompt tuning feature of graphrag, allowing external applications
+to hook into graphrag and generate prompts from private data.
+
+WARNING: This API is under development and may undergo changes in future releases.
+Backwards compatibility is not guaranteed at this time.
+"""
