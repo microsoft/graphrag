838a59180a5141b8fb528c1efa89c61af1fbfe30
Dayenne Souza <ddesouza@microsoft.com>
2024-08-06 18:54:37 -0300

    increase timeout

--- tests/smoke/test_fixtures.py
+++ tests/smoke/test_fixtures.py
@@ -255,7 +255,7 @@ class TestIndexer:
         },
         clear=True,
     )
-    @pytest.mark.timeout(600)  # Extend the timeout to 600 seconds (10 minutes)
+    @pytest.mark.timeout(800)  # Extend the timeout to 600 seconds (10 minutes)
     def test_fixture(
         self,
         input_path: str,
