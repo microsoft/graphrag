f01e0afc0b1204debc0ee4357d45231c127bfdaa
Nathan Evans <github@talkswithnumbers.com>
2024-08-13 10:21:11 -0700

    Fix for windows paths

--- tests/unit/query/test_infer_data_dir.py
+++ tests/unit/query/test_infer_data_dir.py
@@ -10,21 +10,20 @@ from graphrag.query.cli import _infer_data_dir
 def test_infer_data_dir():
     root = "./tests/unit/query/data/defaults"
     result = Path(_infer_data_dir(root))
-    assert result.match("20240812-121000/artifacts") is True
+    assert result.parts[-2] == "20240812-121000"
 
 
 def test_infer_data_dir_ignores_hidden_files():
     """A hidden file, starting with '.', will naturally be selected as latest data directory."""
     root = "./tests/unit/query/data/hidden"
     result = Path(_infer_data_dir(root))
-    print(result)
-    assert result.match("20240812-121000/artifacts") is True
+    assert result.parts[-2] == "20240812-121000"
 
 
 def test_infer_data_dir_ignores_non_numeric():
     root = "./tests/unit/query/data/non-numeric"
     result = Path(_infer_data_dir(root))
-    assert result.match("20240812-121000/artifacts") is True
+    assert result.parts[-2] == "20240812-121000"
 
 
 def test_infer_data_dir_throws_on_no_match():
