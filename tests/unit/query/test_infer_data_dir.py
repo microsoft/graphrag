210ed7e1d2b138fb07a2b53672e0d26d4c76c660
Nathan Evans <github@talkswithnumbers.com>
2024-08-13 09:15:53 -0700

    Format

--- tests/unit/query/test_infer_data_dir.py
+++ tests/unit/query/test_infer_data_dir.py
@@ -12,19 +12,22 @@ def test_infer_data_dir():
     result = Path(_infer_data_dir(root))
     assert result.match("20240812-121000/artifacts") is True
 
+
 def test_infer_data_dir_ignores_hidden_files():
-    """ A hidden file, starting with '.', will naturally be selected as latest data directory."""
+    """A hidden file, starting with '.', will naturally be selected as latest data directory."""
     root = "./tests/unit/query/data/hidden"
     result = Path(_infer_data_dir(root))
     print(result)
     assert result.match("20240812-121000/artifacts") is True
+
 
 def test_infer_data_dir_ignores_non_numeric():
     root = "./tests/unit/query/data/non-numeric"
     result = Path(_infer_data_dir(root))
     assert result.match("20240812-121000/artifacts") is True
 
+
 def test_infer_data_dir_throws_on_no_match():
     root = "./tests/unit/query/data/empty"
-    with pytest.raises(ValueError): # noqa PT011 (this is what is actually thrown...)
+    with pytest.raises(ValueError):  # noqa PT011 (this is what is actually thrown...)
         _infer_data_dir(root)
