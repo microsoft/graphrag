529a0f0c703a1354c6562c03cb77033eca8799b4
Alonso Guevara <alonsog@microsoft.com>
2024-08-14 13:10:30 -0600

    Add support for both float and int on schema validation for community report generation

--- graphrag/index/utils/dicts.py
+++ graphrag/index/utils/dicts.py
@@ -5,7 +5,7 @@
 
 
 def dict_has_keys_with_types(
-    data: dict, expected_fields: list[tuple[str, type]]
+    data: dict, expected_fields: list[tuple[str, type | list[type]]]
 ) -> bool:
     """Return True if the given dictionary has the given keys with the given types."""
     for field, field_type in expected_fields:
@@ -13,6 +13,9 @@ def dict_has_keys_with_types(
             return False
 
         value = data[field]
-        if not isinstance(value, field_type):
+        if isinstance(field_type, list):
+            if not any(isinstance(value, t) for t in field_type):
+                return False
+        elif not isinstance(value, field_type):
             return False
     return True
