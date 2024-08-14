a0ace8ffba45cf5668908dfd082833d773623907
Kanishk Tyagi <kanishktyagi@Kanishks-MacBook-Pro.local>
2024-08-14 11:45:51 +0530

    fix for issue 515

--- graphrag/prompt_tune/prompt/entity_relationship.py
+++ graphrag/prompt_tune/prompt/entity_relationship.py
@@ -22,7 +22,7 @@ For each pair of related entities, extract the following information:
 - relationship_strength: an integer score between 1 to 10, indicating strength of the relationship between the source entity and target entity
 Format each relationship as ("relationship"{{tuple_delimiter}}<source_entity>{{tuple_delimiter}}<target_entity>{{tuple_delimiter}}<relationship_description>{{tuple_delimiter}}<relationship_strength>)
 
-3. Return output in {language} as a single list of all the entities and relationships identified in steps 1 and 2. Use **{{record_delimiter}}** as the list delimiter.
+3. Return output in {language} as a single list of all the entities and relationships identified in steps 1 and 2. Use {{record_delimiter}} as the list delimiter.
 
 4. If you have to translate into {language}, just translate the descriptions, nothing else!
 
