a399dde97b689a5b5c62dc2e9c2290cb2503b3a4
Alonso Guevara <alonsog@microsoft.com>
2024-08-22 10:36:41 -0600

    Merge from main

--- graphrag/llm/openai/openai_chat_llm.py
+++ graphrag/llm/openai/openai_chat_llm.py
@@ -16,7 +16,6 @@ from graphrag.llm.types import (
     LLMOutput,
 )
 
-from ._json import clean_up_json
 from ._prompts import JSON_CHECK_PROMPT
 from .openai_configuration import OpenAIConfiguration
 from .types import OpenAIClientTypes
@@ -104,11 +103,10 @@ class OpenAIChatLLM(BaseLLM[CompletionInput, CompletionOutput]):
             },
         )
 
-        raw_output = result.output or ""
-        json_output = try_parse_json_object(raw_output)
+        output, json_output = try_parse_json_object(result.output or "")
 
         return LLMOutput[CompletionOutput](
-            output=raw_output,
+            output=output,
             json=json_output,
             history=result.history,
         )
@@ -119,24 +117,22 @@ class OpenAIChatLLM(BaseLLM[CompletionInput, CompletionOutput]):
         # Otherwise, clean up the output and try to parse it as json
         result = await self._invoke(input, **kwargs)
         history = result.history or []
-        output = clean_up_json(result.output or "")
-        try:
-            json_output = try_parse_json_object(output)
+        output, json_output = try_parse_json_object(result.output or "")
+        if json_output:
             return LLMOutput[CompletionOutput](
-                output=output, json=json_output, history=history
+                output=result.output, json=json_output, history=history
             )
-        except (TypeError, JSONDecodeError):
-            log.warning("error parsing llm json, retrying")
-            # If cleaned up json is unparsable, use the LLM to reformat it (may throw)
-            result = await self._try_clean_json_with_llm(output, **kwargs)
-            output = clean_up_json(result.output or "")
-            json = try_parse_json_object(output)
+        # if not return correct formatted json, retry 
+        log.warning("error parsing llm json, retrying")
+        # If cleaned up json is unparsable, use the LLM to reformat it (may throw)
+        result = await self._try_clean_json_with_llm(output, **kwargs)
+        output, json_output = try_parse_json_object(result.output or "")
 
-            return LLMOutput[CompletionOutput](
-                output=output,
-                json=json,
-                history=history,
-            )
+        return LLMOutput[CompletionOutput](
+            output=output,
+            json=json_output,
+            history=history,
+        )
 
     async def _try_clean_json_with_llm(
         self, output: str, **kwargs: Unpack[LLMInput]
