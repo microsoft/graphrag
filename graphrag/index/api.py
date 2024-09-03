f02a8ef29c0f70e2e8b1fcaee731d97bb2e98865
Alonso Guevara <alonsog@microsoft.com>
2024-09-03 16:58:35 -0600

    Fix ruff

--- graphrag/index/api.py
+++ graphrag/index/api.py
@@ -8,8 +8,6 @@ WARNING: This API is under development and may undergo changes in future release
 Backwards compatibility is not guaranteed at this time.
 """
 
-from pathlib import Path
-
 from graphrag.config import CacheType, GraphRagConfig
 
 from .cache.noop_pipeline_cache import NoopPipelineCache
@@ -56,7 +54,6 @@ async def build_index(
     list[PipelineRunResult]
         The list of pipeline run results
     """
-
     pipeline_config = create_pipeline_config(config)
     pipeline_cache = (
         NoopPipelineCache() if config.cache.type == CacheType.none is None else None
