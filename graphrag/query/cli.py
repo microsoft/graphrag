52146b3a91c1bb63d395a3bdc89d58dcdcda875d
Kenny Zhang <zhangken@microsoft.com>
2024-09-05 13:28:57 -0400

    rewrote function to use storage utils

--- graphrag/query/cli.py
+++ graphrag/query/cli.py
@@ -8,18 +8,15 @@ import sys
 from pathlib import Path
 
 import pandas as pd
-from azure.identity import DefaultAzureCredential as DefaultAzureCredentialSync
-from azure.identity.aio import DefaultAzureCredential as DefaultAzureCredentialAsync
-from azure.storage.blob import BlobServiceClient
 
 from graphrag.config import (
     GraphRagConfig,
-    StorageType,
-    create_graphrag_config,
     load_config,
     resolve_path,
 )
+from graphrag.index.create_pipeline_config import create_pipeline_config
 from graphrag.index.progress import PrintProgressReporter
+from graphrag.utils.storage import _create_storage, _load_table_from_storage
 
 from . import api
 
@@ -47,10 +44,21 @@ def run_global_search(
 
     data_path = Path(config.storage.base_dir).resolve()
 
-    dataframe_dict = _resolve_parquet_files(data_path=data_path, config=config)
-    final_nodes: pd.DataFrame = dataframe_dict["nodes"]
-    final_entities: pd.DataFrame = dataframe_dict["entities"]
-    final_community_reports: pd.DataFrame = dataframe_dict["community_reports"]
+    dataframe_dict = _resolve_parquet_files(
+        root_dir=root_dir,
+        config=config,
+        parquet_list=[
+            "create_final_nodes.parquet",
+            "create_final_entities.parquet",
+            "create_final_community_reports.parquet",
+        ],
+        optional_list=[],
+    )
+    final_nodes: pd.DataFrame = dataframe_dict["create_final_nodes"]
+    final_entities: pd.DataFrame = dataframe_dict["create_final_entities"]
+    final_community_reports: pd.DataFrame = dataframe_dict[
+        "create_final_community_reports"
+    ]
 
     # call the Query API
     if streaming:
@@ -118,13 +126,26 @@ def run_local_search(
 
     data_path = Path(config.storage.base_dir).resolve()
 
-    dataframe_dict = _resolve_parquet_files(data_path=data_path, config=config)
-    final_nodes: pd.DataFrame = dataframe_dict["nodes"]
-    final_community_reports: pd.DataFrame = dataframe_dict["community_reports"]
-    final_text_units: pd.DataFrame = dataframe_dict["text_units"]
-    final_relationships: pd.DataFrame = dataframe_dict["relationships"]
-    final_entities: pd.DataFrame = dataframe_dict["entities"]
-    final_covariates: pd.DataFrame | None = dataframe_dict["covariates"]
+    dataframe_dict = _resolve_parquet_files(
+        root_dir=root_dir,
+        config=config,
+        parquet_list=[
+            "create_final_nodes.parquet",
+            "create_final_community_reports.parquet",
+            "create_final_text_units.parquet",
+            "create_final_relationships.parquet",
+            "create_final_entities.parquet",
+        ],
+        optional_list=["create_final_covariates.parquet"],
+    )
+    final_nodes: pd.DataFrame = dataframe_dict["create_final_nodes"]
+    final_community_reports: pd.DataFrame = dataframe_dict[
+        "create_final_community_reports"
+    ]
+    final_text_units: pd.DataFrame = dataframe_dict["create_final_text_units"]
+    final_relationships: pd.DataFrame = dataframe_dict["create_final_relationships"]
+    final_entities: pd.DataFrame = dataframe_dict["create_final_entities"]
+    final_covariates: pd.DataFrame | None = dataframe_dict["create_final_covariates"]
 
     # call the Query API
     if streaming:
@@ -178,181 +199,32 @@ def run_local_search(
 
 
 def _resolve_parquet_files(
-    data_path: Path, config: GraphRagConfig
+    root_dir: str,
+    config: GraphRagConfig,
+    parquet_list: list[str],
+    optional_list: list[str],
 ) -> dict[str, pd.DataFrame]:
     """Read parquet files to a dataframe dict."""
     dataframe_dict = {}
-    match config.storage.type:
-        case StorageType.blob:
-            if config.storage.container_name is None:
-                msg = "Container name required for querying from blob storage"
-                raise ValueError(msg)
-            if (
-                config.storage.connection_string is None
-                and config.storage.storage_account_blob_url is None
-            ):
-                msg = "Connection string or storage account blob url required for querying blob storage"
-                raise ValueError(msg)
+    pipeline_config = create_pipeline_config(config)
+    storage_obj = _create_storage(root_dir=root_dir, config=pipeline_config.storage)
+    for parquet_file in parquet_list:
+        df_key = parquet_file.split(".")[0]
+        df_value = asyncio.run(
+            _load_table_from_storage(name=parquet_file, storage=storage_obj)
+        )
+        dataframe_dict[df_key] = df_value
 
-            if config.storage.storage_account_blob_url is not None:
-                storage_account_blob_url = str(config.storage.storage_account_blob_url)
-                storage_account_name = storage_account_blob_url.split("//")[1].split(
-                    "."
-                )[0]
+    # for optional parquet files, set the dict entry to None instead of erroring out if it does not exist
+    for optional_file in optional_list:
+        file_exists = asyncio.run(storage_obj.has(optional_file))
+        df_key = optional_file.split(".")[0]
+        if file_exists:
+            df_value = asyncio.run(
+                _load_table_from_storage(name=optional_file, storage=storage_obj)
+            )
+            dataframe_dict[df_key] = df_value
+        else:
+            dataframe_dict[df_key] = None
 
-                storage_options = {
-                    "account_name": storage_account_name,
-                    "credential": DefaultAzureCredentialAsync(),
-                }
-
-                blob_service_client = BlobServiceClient(
-                    account_url=storage_account_blob_url,
-                    credential=DefaultAzureCredentialSync(),
-                )
-
-            else:
-                connection_string = str(config.storage.connection_string)
-                storage_account_name = connection_string.split("AccountName=")[1].split(
-                    ";"
-                )[0]
-                storage_options = {
-                    "account_name": storage_account_name,
-                    "connection_string": connection_string,
-                }
-
-                blob_service_client = BlobServiceClient.from_connection_string(
-                    conn_str=connection_string
-                )
-
-            container_name = config.storage.container_name
-            base_dir = config.storage.base_dir
-
-            def get_abfs_path(parquet_name: str):
-                abfs_path_str = str(Path(container_name) / base_dir / parquet_name)
-                return f"abfs://{abfs_path_str}"
-
-            container_client = blob_service_client.get_container_client(container_name)
-            covariates_exists = container_client.get_blob_client(
-                f"{base_dir}/create_final_covariates.parquet"
-            ).exists()
-            if covariates_exists:
-                dataframe_dict["covariates"] = pd.read_parquet(
-                    path=get_abfs_path("create_final_covariates.parquet"),
-                    storage_options=storage_options,
-                )
-            else:
-                dataframe_dict["covariates"] = None
-
-            dataframe_dict["nodes"] = pd.read_parquet(
-                path=get_abfs_path("create_final_nodes.parquet"),
-                storage_options=storage_options,
-            )
-            dataframe_dict["entities"] = pd.read_parquet(
-                path=get_abfs_path("create_final_entities.parquet"),
-                storage_options=storage_options,
-            )
-            dataframe_dict["community_reports"] = pd.read_parquet(
-                path=get_abfs_path("create_final_community_reports.parquet"),
-                storage_options=storage_options,
-            )
-            dataframe_dict["text_units"] = pd.read_parquet(
-                path=get_abfs_path("create_final_text_units.parquet"),
-                storage_options=storage_options,
-            )
-            dataframe_dict["relationships"] = pd.read_parquet(
-                path=get_abfs_path("create_final_relationships.parquet"),
-                storage_options=storage_options,
-            )
-
-        case StorageType.file:
-            dataframe_dict["nodes"] = pd.read_parquet(
-                data_path / "create_final_nodes.parquet"
-            )
-            dataframe_dict["entities"] = pd.read_parquet(
-                data_path / "create_final_entities.parquet"
-            )
-            dataframe_dict["community_reports"] = pd.read_parquet(
-                data_path / "create_final_community_reports.parquet"
-            )
-            dataframe_dict["text_units"] = pd.read_parquet(
-                data_path / "create_final_text_units.parquet"
-            )
-            dataframe_dict["relationships"] = pd.read_parquet(
-                data_path / "create_final_relationships.parquet"
-            )
-
-            final_covariates_path = data_path / "create_final_covariates.parquet"
-            dataframe_dict["covariates"] = (
-                pd.read_parquet(final_covariates_path)
-                if final_covariates_path.exists()
-                else None
-            )
-        case _:
-            dataframe_dict["nodes"] = pd.read_parquet(
-                data_path / "create_final_nodes.parquet"
-            )
-            dataframe_dict["entities"] = pd.read_parquet(
-                data_path / "create_final_entities.parquet"
-            )
-            dataframe_dict["community_reports"] = pd.read_parquet(
-                data_path / "create_final_community_reports.parquet"
-            )
-            dataframe_dict["text_units"] = pd.read_parquet(
-                data_path / "create_final_text_units.parquet"
-            )
-            dataframe_dict["relationships"] = pd.read_parquet(
-                data_path / "create_final_relationships.parquet"
-            )
-
-            final_covariates_path = data_path / "create_final_covariates.parquet"
-            dataframe_dict["covariates"] = (
-                pd.read_parquet(final_covariates_path)
-                if final_covariates_path.exists()
-                else None
-            )
     return dataframe_dict
-
-
-def _create_graphrag_config(
-    root: str | None,
-    config_filepath: str | None,
-) -> GraphRagConfig:
-    """Create a GraphRag configuration."""
-    return _read_config_parameters(root or "./", config_filepath)
-
-
-def _read_config_parameters(root: str, config: str | None):
-    _root = Path(root)
-    settings_yaml = (
-        Path(config)
-        if config and Path(config).suffix in [".yaml", ".yml"]
-        else _root / "settings.yaml"
-    )
-    if not settings_yaml.exists():
-        settings_yaml = _root / "settings.yml"
-
-    if settings_yaml.exists():
-        reporter.info(f"Reading settings from {settings_yaml}")
-        with settings_yaml.open(
-            "rb",
-        ) as file:
-            import yaml
-
-            data = yaml.safe_load(file.read().decode(encoding="utf-8", errors="strict"))
-            return create_graphrag_config(data, root)
-
-    settings_json = (
-        Path(config)
-        if config and Path(config).suffix == ".json"
-        else _root / "settings.json"
-    )
-    if settings_json.exists():
-        reporter.info(f"Reading settings from {settings_json}")
-        with settings_json.open("rb") as file:
-            import json
-
-            data = json.loads(file.read().decode(encoding="utf-8", errors="strict"))
-            return create_graphrag_config(data, root)
-
-    reporter.info("Reading settings from environment variables")
-    return create_graphrag_config(root_dir=root)
