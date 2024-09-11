8163f743bbdb76176404d0a3c7f19fc846815519
Alonso Guevara <alonsog@microsoft.com>
2024-09-11 15:42:11 -0600

    Fix summarization including empty descriptions

--- graphrag/index/graph/extractors/summarize/description_summary_extractor.py
+++ graphrag/index/graph/extractors/summarize/description_summary_extractor.py
@@ -87,7 +87,11 @@ class SummarizeExtractor:
         if not isinstance(descriptions, list):
             descriptions = [descriptions]
 
-            # Iterate over descriptions, adding all until the max input tokens is reached
+        # Sort description lists
+        if len(descriptions) > 1:
+            descriptions = sorted(descriptions)
+
+        # Iterate over descriptions, adding all until the max input tokens is reached
         usable_tokens = self._max_input_tokens - num_tokens_from_string(
             self._summarization_prompt
         )
