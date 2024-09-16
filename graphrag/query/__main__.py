38254efec4ce7b448808728118971260b5051d78
Sirus Sh <sirus.sh@outlook.com>
2024-09-16 16:21:33 -0700

    Add missing args

--- graphrag/query/__main__.py
+++ graphrag/query/__main__.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
                 args.query[0],
                 optimized_search=args.optimized_search,
                 use_kusto_community_reports=args.use_kusto_community_reports,
-                paths=paths,
+                paths=args.paths,
             )
         case SearchType.GLOBAL:
             run_global_search(
