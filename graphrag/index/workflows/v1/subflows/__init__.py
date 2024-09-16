d56ca107d6b73c6320a2152c19455325a4f2148a
Nathan Evans <github@talkswithnumbers.com>
2024-09-16 12:09:31 -0700

    Format

--- graphrag/index/workflows/v1/subflows/__init__.py
+++ graphrag/index/workflows/v1/subflows/__init__.py
@@ -6,6 +6,7 @@
 from .join_text_units_to_covariate_ids import join_text_units_to_covariate_ids
 from .join_text_units_to_entity_ids import join_text_units_to_entity_ids
 from .join_text_units_to_relationship_ids import join_text_units_to_relationship_ids
+
 __all__ = [
     "join_text_units_to_covariate_ids",
     "join_text_units_to_entity_ids",
