d22c0e7836f7837bf9cccbfd912f636b3c323600
Nathan Evans <github@talkswithnumbers.com>
2024-09-16 12:35:45 -0700

    Covariate collapse (#1142)
    * Setup basic verb test runner
    
    * Replace join_text_units_to_entity_ids with subflow
    
    * Update comments
    
    * Replace join_text_units_to_relationship_ids subflow
    
    * Roll in final select
    
    * Reuse assertion util
    
    * Small fix + format
    
    * Format/typing
    
    * Semver
    
    * Format/typing
    
    * Semver
    
    * Revert format changes
    
    * Fix smoke test subworkflow count
    
    * Edit subworkflows for another smoke test
    
    * Update test parquets for covariates
    
    * Collapse covariate join
    
    * Rework subtasks for per-flow customization
    
    * Format
    
    * Semver
    
    * Fix smoke test

--- graphrag/index/workflows/v1/subflows/__init__.py
+++ graphrag/index/workflows/v1/subflows/__init__.py
@@ -3,8 +3,12 @@
 
 """The Indexing Engine workflows -> subflows package root."""
 
-from .join_text_units import join_text_units
+from .join_text_units_to_covariate_ids import join_text_units_to_covariate_ids
+from .join_text_units_to_entity_ids import join_text_units_to_entity_ids
+from .join_text_units_to_relationship_ids import join_text_units_to_relationship_ids
 
 __all__ = [
-    "join_text_units",
+    "join_text_units_to_covariate_ids",
+    "join_text_units_to_entity_ids",
+    "join_text_units_to_relationship_ids",
 ]
