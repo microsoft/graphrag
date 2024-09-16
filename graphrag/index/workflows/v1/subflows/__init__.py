abb3d1a5d0ee36f03311b05e7049ddb471cde06d
Nathan Evans <github@talkswithnumbers.com>
2024-09-16 12:02:31 -0700

    Collapse covariate join

--- graphrag/index/workflows/v1/subflows/__init__.py
+++ graphrag/index/workflows/v1/subflows/__init__.py
@@ -4,7 +4,9 @@
 """The Indexing Engine workflows -> subflows package root."""
 
 from .join_text_units import join_text_units
+from .join_text_units_to_covariate_ids import join_text_units_to_covariate_ids
 
 __all__ = [
     "join_text_units",
+    "join_text_units_to_covariate_ids",
 ]
