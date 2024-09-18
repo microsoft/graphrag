aa5b426f1d75c5355a28467650258976ca28e939
Nathan Evans <github@talkswithnumbers.com>
2024-09-17 17:04:42 -0700

    Collapse final communities workflow (#1150)
    * Collapse create_final_communities
    
    * Semver
    
    * Spellcheck
    
    * Clean up filtering
    
    * Add space in title
    
    * Format
    
    * Cleanup imports and format
    
    * Spruce up the tests
    
    * Update dictionary.txt
    
    * Spellcheck
    
    ---------
    
    Co-authored-by: Alonso Guevara <alonsog@microsoft.com>

--- graphrag/index/workflows/v1/subflows/__init__.py
+++ graphrag/index/workflows/v1/subflows/__init__.py
@@ -3,8 +3,10 @@
 
 """The Indexing Engine workflows -> subflows package root."""
 
+from .create_final_communities import create_final_communities
 from .create_final_text_units_pre_embedding import create_final_text_units_pre_embedding
 
 __all__ = [
+    "create_final_communities",
     "create_final_text_units_pre_embedding",
 ]
