1330223c1680f33435036cdd52c8e4febe55f4d9
Nathan Evans <github@talkswithnumbers.com>
2024-09-19 17:01:34 -0700

    Collapse create_final_nodes

--- graphrag/index/workflows/v1/subflows/__init__.py
+++ graphrag/index/workflows/v1/subflows/__init__.py
@@ -4,9 +4,11 @@
 """The Indexing Engine workflows -> subflows package root."""
 
 from .create_final_communities import create_final_communities
+from .create_final_nodes import create_final_nodes
 from .create_final_text_units_pre_embedding import create_final_text_units_pre_embedding
 
 __all__ = [
     "create_final_communities",
+    "create_final_nodes",
     "create_final_text_units_pre_embedding",
 ]
