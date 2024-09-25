0912e150518562b3991458eb7d32135418ed4768
amritpalms <amritpal.lim@gmail.com>
2024-09-25 03:56:22 -0700

    Working Rag on Azure

--- func-app/graphrag/common/storage/blob_pipeline_storage.py
+++ func-app/graphrag/common/storage/blob_pipeline_storage.py
@@ -4,6 +4,7 @@
 """Azure Blob Storage implementation of PipelineStorage."""
 
 import logging
+import os
 import re
 from collections.abc import Iterator
 from pathlib import Path
@@ -50,7 +51,7 @@ class BlobPipelineStorage(PipelineStorage):
 
             self._blob_service_client = BlobServiceClient(
                 account_url=storage_account_blob_url,
-                credential=DefaultAzureCredential(managed_identity_client_id="295ce65c-28c6-4763-be6f-a5eb36c3ceb3"),
+                credential=DefaultAzureCredential(managed_identity_client_id=os.getenv('AZURE_CLIENT_ID'), exclude_interactive_browser_credential = False),
             )
         self._encoding = encoding or "utf-8"
         self._container_name = container_name
@@ -215,7 +216,7 @@ class BlobPipelineStorage(PipelineStorage):
                 self._abfs_url(key),
                 storage_options={
                     "account_name": self._storage_account_name,
-                    "credential": DefaultAzureCredential(),
+                    "credential": DefaultAzureCredential(managed_identity_client_id=os.getenv('AZURE_CLIENT_ID'), exclude_interactive_browser_credential = False),
                 },
                 orient="records",
                 lines=True,
@@ -237,7 +238,7 @@ class BlobPipelineStorage(PipelineStorage):
                 self._abfs_url(key),
                 storage_options={
                     "account_name": self._storage_account_name,
-                    "credential": DefaultAzureCredential(),
+                    "credential": DefaultAzureCredential(managed_identity_client_id=os.getenv('AZURE_CLIENT_ID'), exclude_interactive_browser_credential = False),
                 },
             )
         else:
--- func-app/graphrag/index/cli.py
+++ func-app/graphrag/index/cli.py
@@ -124,8 +124,8 @@ def index_cli(
         #signal.signal(signal.SIGINT, handle_signal)
         
         logging.info("Step3")
-        if sys.platform != "win32":
-            signal.signal(signal.SIGHUP, handle_signal)
+        #if sys.platform != "win32":
+        #    signal.signal(signal.SIGHUP, handle_signal)
 
         logging.info("Step4")
         async def execute():
@@ -167,9 +167,9 @@ def index_cli(
                 runner.run(execute())
         else:
             logging.info("Step 6")
-            import uvloop  # type: ignore Ignoring because on windows this will cause an error
+            #import uvloop  # type: ignore Ignoring because on windows this will cause an error
 
-            uvloop.install()
+            #uvloop.install()
             asyncio.run(execute())
 
     _run_workflow_async()
@@ -182,7 +182,8 @@ def index_cli(
         progress_reporter.success("All workflows completed successfully.")
 
     if cli:
-        sys.exit(1 if encountered_errors else 0)
+        #sys.exit(1 if encountered_errors else 0)
+        return 0
 
 def _switch_context(root: str, config: str,
                     reporter: ProgressReporter, context_operation: str | None,
@@ -219,42 +220,23 @@ def _initialize_project_at(path: str, reporter: ProgressReporter) -> None:
     settings_yaml = root / "settings/settings.yaml"
     
     dotenv = root / ".env"
-    if not dotenv.exists():
-        with settings_yaml.open("wb") as file:
-            file.write(INIT_YAML.encode(encoding="utf-8", errors="strict"))
+    
 
-    with dotenv.open("wb") as file:
-        file.write(INIT_DOTENV.encode(encoding="utf-8", errors="strict"))
+    
 
     prompts_dir = root / "prompts"
-    if not prompts_dir.exists():
-        prompts_dir.mkdir(parents=True, exist_ok=True)
+    
 
     entity_extraction = prompts_dir / "entity_extraction.txt"
-    if not entity_extraction.exists():
-        with entity_extraction.open("wb") as file:
-            file.write(
-                GRAPH_EXTRACTION_PROMPT.encode(encoding="utf-8", errors="strict")
-            )
+    
 
     summarize_descriptions = prompts_dir / "summarize_descriptions.txt"
-    if not summarize_descriptions.exists():
-        with summarize_descriptions.open("wb") as file:
-            file.write(SUMMARIZE_PROMPT.encode(encoding="utf-8", errors="strict"))
 
     claim_extraction = prompts_dir / "claim_extraction.txt"
-    if not claim_extraction.exists():
-        with claim_extraction.open("wb") as file:
-            file.write(
-                CLAIM_EXTRACTION_PROMPT.encode(encoding="utf-8", errors="strict")
-            )
+    
 
     community_report = prompts_dir / "community_report.txt"
-    if not community_report.exists():
-        with community_report.open("wb") as file:
-            file.write(
-                COMMUNITY_REPORT_PROMPT.encode(encoding="utf-8", errors="strict")
-            )
+    
 
 
 def _create_default_config(
@@ -287,7 +269,7 @@ def _create_default_config(
 
     if dryrun:
         reporter.info("dry run complete, exiting...")
-        sys.exit(0)
+        return
     return result
 
 
@@ -342,16 +324,16 @@ def _enable_logging(root_dir: str, run_id: str, verbose: bool) -> None:
     logging_file = (
         Path(root_dir) / "output" / run_id / "reports" / "indexing-engine.log"
     )
-    logging_file.parent.mkdir(parents=True, exist_ok=True)
+    #logging_file.parent.mkdir(parents=True, exist_ok=True)
 
-    logging_file.touch(exist_ok=True)
+    #logging_file.touch(exist_ok=True)
     handler = logging.StreamHandler(stream=sys.stdout)
-    fileHandler = logging.FileHandler(logging_file, mode="a")
+    #fileHandler = logging.FileHandler(logging_file, mode="a")
     logging.basicConfig(
         #filename=str(logging_file),
         #filemode="a",
         format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
         datefmt="%H:%M:%S",
         level=logging.DEBUG if verbose else logging.INFO,
-        handlers=[handler, fileHandler]
+        handlers=[handler]
     )
