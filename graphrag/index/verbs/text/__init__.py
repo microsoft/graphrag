467faed8cc692cd9262b9675e9623205f8373e4b
Nathan Evans <github@talkswithnumbers.com>
2024-10-03 14:02:34 -0700

    Delete text replace

--- graphrag/index/verbs/text/__init__.py
+++ graphrag/index/verbs/text/__init__.py
@@ -5,13 +5,11 @@
 
 from .chunk.text_chunk import chunk
 from .embed import text_embed
-from .replace import replace
 from .split import text_split
 from .translate import text_translate
 
 __all__ = [
     "chunk",
-    "replace",
     "text_embed",
     "text_split",
     "text_translate",
