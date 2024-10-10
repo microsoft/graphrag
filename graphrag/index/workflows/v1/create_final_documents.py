872ad0fb059f3df80a0ca20404da509a97cba657
Nathan Evans <github@talkswithnumbers.com>
2024-10-10 09:55:14 -0700

    Copy base documents logic into final documents

--- graphrag/index/workflows/v1/create_final_documents.py
+++ graphrag/index/workflows/v1/create_final_documents.py
@@ -3,6 +3,8 @@
 
 """A module containing build_steps method definition."""
 
+from datashaper import DEFAULT_INPUT_NAME
+
 from graphrag.index.config import PipelineWorkflowConfig, PipelineWorkflowStep
 
 workflow_name = "create_final_documents"
@@ -22,14 +24,19 @@ def build_steps(
         "document_raw_content_embed", base_text_embed
     )
     skip_raw_content_embedding = config.get("skip_raw_content_embedding", False)
+    document_attribute_columns = config.get("document_attribute_columns", [])
     return [
         {
             "verb": "create_final_documents",
             "args": {
+                "document_attribute_columns": document_attribute_columns,
                 "raw_content_text_embed": document_raw_content_embed_config
                 if not skip_raw_content_embedding
                 else None,
             },
-            "input": {"source": "workflow:create_base_documents"},
+            "input": {
+                "source": DEFAULT_INPUT_NAME,
+                "text_units": "workflow:create_final_text_units",
+            },
         },
     ]
