adccd8db2eb5786a11f0129685c10e8213412fd6
王欣欣 <wangxinxin@wangxinxindeMacBook-Pro.local>
2024-10-11 16:36:52 +0800

    fixbug and support for ollama and other llms

--- graphrag/query/llm/text_utils.py
+++ graphrag/query/llm/text_utils.py
@@ -38,5 +38,7 @@ def chunk_text(
     if token_encoder is None:
         token_encoder = tiktoken.get_encoding("cl100k_base")
     tokens = token_encoder.encode(text)  # type: ignore
+    tokens = token_encoder.decode(tokens) # Decoding tokens into strings
+
     chunk_iterator = batched(iter(tokens), max_tokens)
-    yield from (token_encoder.decode(list(chunk)) for chunk in chunk_iterator)
+    yield from chunk_iterator
