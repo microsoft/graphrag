92c1ef27699dde6b7293886d4782b054f8f1a04a
Josh Bradley <joshbradley@microsoft.com>
2024-10-22 19:03:45 -0400

    cast test filepaths to str

--- tests/smoke/test_fixtures.py
+++ tests/smoke/test_fixtures.py
@@ -136,7 +136,7 @@ class TestIndexer:
             "index",
             "--verbose" if debug else None,
             "--root",
-            root.absolute().resolve(),
+            str(root.absolute().resolve()),
             "--reporter",
             "print",
         ]
@@ -233,7 +233,7 @@ class TestIndexer:
             "poe",
             "query",
             "--root",
-            root.absolute().resolve(),
+            str(root.absolute().resolve()),
             "--method",
             query_config["method"],
             "--community-level",
