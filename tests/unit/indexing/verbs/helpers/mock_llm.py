c165f85da4917f1e1300f6195b38bda0c5425260
Chris Trevino <chtrevin@microsoft.com>
2024-10-23 13:25:08 -0700

    update mockllm

--- tests/unit/indexing/verbs/helpers/mock_llm.py
+++ tests/unit/indexing/verbs/helpers/mock_llm.py
@@ -1,10 +1,27 @@
 # Copyright (c) 2024 Microsoft Corporation.
 # Licensed under the MIT License
-from graphrag.llm import CompletionLLM, MockChatLLM
+from fnllm import LLM, LLMInput, LLMOutput
+from fnllm.types.generics import THistoryEntry, TJsonModel, TModelParameters
+from typing_extensions import Unpack
+
+
+class MockChatLLM(LLM):
+    def __init__(self, responses: list[str]):
+        self.responses = responses
+        self.response_index = 0
+
+    async def __call__(
+        self,
+        prompt: str,
+        **kwargs: Unpack[LLMInput[TJsonModel, THistoryEntry, TModelParameters]],
+    ) -> LLMOutput[str, TJsonModel, THistoryEntry]:
+        response = self.responses[self.response_index]
+        self.response_index += 1
+        return LLMOutput(output=response)
 
 
 def create_mock_llm(
     responses: list[str],
-) -> CompletionLLM:
+) -> LLM:
     """Creates a mock LLM that returns the given responses."""
     return MockChatLLM(responses)
