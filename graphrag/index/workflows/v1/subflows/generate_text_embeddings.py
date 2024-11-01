93e6fa33d60dfa3582c774058f668f9ea044ecbd
Josh Bradley <joshbradley@microsoft.com>
2024-11-01 19:41:12 -0400

    fix import

--- graphrag/index/workflows/v1/subflows/generate_text_embeddings.py
+++ graphrag/index/workflows/v1/subflows/generate_text_embeddings.py
@@ -16,11 +16,11 @@ from datashaper import (
     verb,
 )
 
-from graphrag.index.cache import PipelineCache
+from graphrag.index.cache.pipeline_cache import PipelineCache
 from graphrag.index.flows.generate_text_embeddings import (
     generate_text_embeddings as generate_text_embeddings_flow,
 )
-from graphrag.index.storage import PipelineStorage
+from graphrag.index.storage.pipeline_storage import PipelineStorage
 from graphrag.index.utils.ds_util import get_required_input_table
 
 log = logging.getLogger(__name__)
