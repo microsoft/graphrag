4f4098b0005bc08d04126ffd32d058c6ff8800b5
Josh Bradley <joshbradley@microsoft.com>
2024-12-09 01:48:24 -0800

    add comment

--- graphrag/logger/types.py
+++ graphrag/logger/types.py
@@ -1,11 +1,15 @@
 # Copyright (c) 2024 Microsoft Corporation.
 # Licensed under the MIT License
 
-"""Logging types."""
+"""Logging types.
+
+This module defines the types of loggers that can be used.
+"""
 
 from enum import Enum
 
 
+# Note: Code in this module was not included in the factory module because it negatively impacts the CLI experience.
 class LoggerType(str, Enum):
     """The type of logger to use."""
 
