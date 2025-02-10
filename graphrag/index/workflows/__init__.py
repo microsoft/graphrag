d6169d9cae707b1ba35beabc1b7ba27ef230b700
Nathan Evans <github@talkswithnumbers.com>
2025-02-10 12:22:53 -0800

    Add workflow registration

--- graphrag/index/workflows/__init__.py
+++ graphrag/index/workflows/__init__.py
@@ -4,7 +4,7 @@
 
 """A package containing all built-in workflow definitions."""
 
-from graphrag.index.typing import WorkflowFunction
+from graphrag.index.workflows.factory import PipelineFactory
 
 from .create_base_text_units import (
     run_workflow as run_create_base_text_units,
@@ -67,10 +67,8 @@ from .generate_text_embeddings import (
     workflow_name as generate_text_embeddings,
 )
 
-all_workflows: dict[
-    str,
-    WorkflowFunction,
-] = {
+# register all of our built-in workflows at once
+PipelineFactory.register_all({
     create_base_text_units: run_create_base_text_units,
     create_communities: run_create_communities,
     create_community_reports_text: run_create_community_reports_text,
@@ -81,5 +79,4 @@ all_workflows: dict[
     extract_graph_nlp: run_extract_graph_nlp,
     extract_graph: run_extract_graph,
     generate_text_embeddings: run_generate_text_embeddings,
-}
-"""This is a dictionary of all build-in workflows. To be replace with an injectable provider!"""
+})
