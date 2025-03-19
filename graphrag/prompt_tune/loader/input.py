3bcc028c54c57cb22ba5c79558b5099d686133bd
Gabriel Nieves <gnievesponce@microsoft.com>
2025-03-19 20:48:50 +0000

    Eliminated redunant code by using the embed_text strategy directly

--- graphrag/prompt_tune/loader/input.py
+++ graphrag/prompt_tune/loader/input.py
@@ -3,23 +3,17 @@
 
 """Input loading module."""
 
-import asyncio
-
 import numpy as np
 import pandas as pd
 
+from graphrag.cache.noop_pipeline_cache import NoopPipelineCache
 from graphrag.callbacks.noop_workflow_callbacks import NoopWorkflowCallbacks
 from graphrag.config.models.graph_rag_config import GraphRagConfig
-from graphrag.config.models.language_model_config import LanguageModelConfig
 from graphrag.index.input.factory import create_input
 from graphrag.index.operations.embed_text.strategies.openai import (
-    _create_text_batches,
-    _prepare_embed_texts,
+    run as run_embed_text,
 )
-from graphrag.index.text_splitting.text_splitting import TokenTextSplitter
 from graphrag.index.workflows.create_base_text_units import create_base_text_units
-from graphrag.language_model.manager import ModelManager
-from graphrag.language_model.protocol.base import EmbeddingModel
 from graphrag.logger.base import ProgressLogger
 from graphrag.prompt_tune.defaults import (
     LIMIT,
@@ -29,57 +23,9 @@ from graphrag.prompt_tune.defaults import (
 from graphrag.prompt_tune.types import DocSelectionType
 
 
-async def _embed(
-    model: EmbeddingModel, chunk: list[str], semaphore: asyncio.Semaphore
-) -> np.ndarray[float, np.dtype[np.float_]]:
-    async with semaphore:
-        chunk_embeddings = await model.aembed_batch(chunk)
-        return np.array(chunk_embeddings)
-
-
-async def _embed_chunks(
-    batch_size: int,
-    batch_max_tokens: int,
-    text_chunks: pd.DataFrame,
-    embedding_llm: EmbeddingModel,
-    config: LanguageModelConfig,
-    splitter: TokenTextSplitter,
-    logger: ProgressLogger,
-    n_subset_max: int = N_SUBSET_MAX,
-) -> tuple[pd.DataFrame, np.ndarray]:
-    """Convert text chunks into dense text embeddings."""
-    sampled_text_chunks = text_chunks.sample(n=min(n_subset_max, len(text_chunks)))[
-        "text"
-    ].tolist()
-    preped_text_chunks, _ = _prepare_embed_texts(sampled_text_chunks, splitter)
-    semaphore: asyncio.Semaphore = asyncio.Semaphore(config.concurrent_requests)
-
-    # Break up the input texts. The sizes here indicate how many snippets are in each input text
-    sampled_batches = _create_text_batches(
-        preped_text_chunks,
-        batch_size,
-        batch_max_tokens,
-        splitter,
-    )
-    logger.info(
-        (  # noqa: UP034
-            f"embedding {len(sampled_text_chunks)} inputs "  # noqa: G004
-            f"via {len(preped_text_chunks)} snippets "
-            f"using {len(sampled_batches)} batches. "
-            f"max_batch_size={batch_size}, max_tokens={batch_max_tokens}"
-        )
-    )
-
-    # Embed each chunk of snippets
-    futures = [_embed(embedding_llm, batch, semaphore) for batch in sampled_batches]
-    embeddings = await asyncio.gather(*futures)
-    # merge results in a single list of lists (reduce the collect dimension)
-    return text_chunks, np.array([item for sublist in embeddings for item in sublist])
-
-
 def _sample_chunks_from_embeddings(
     text_chunks: pd.DataFrame,
-    embeddings,
+    embeddings: np.ndarray[float, np.dtype[np.float_]],
     k: int = K,
 ) -> pd.DataFrame:
     """Sample text chunks from embeddings."""
@@ -105,13 +51,6 @@ async def load_docs_in_chunks(
     embeddings_llm_settings = config.get_language_model_config(
         config.embed_text.model_id
     )
-    batch_size = config.embed_text.batch_size
-    batch_max_tokens = config.embed_text.batch_max_tokens
-    splitter = TokenTextSplitter(
-        encoding_name=embeddings_llm_settings.encoding_model,
-        chunk_size=batch_max_tokens,
-    )
-
     dataset = await create_input(config.input, logger, root)
     chunk_config = config.chunks
     chunks_df = create_base_text_units(
@@ -139,24 +78,24 @@ async def load_docs_in_chunks(
         if k is None or k <= 0:
             msg = "k must be an integer > 0"
             raise ValueError(msg)
-        embedding_llm = ModelManager().register_embedding(
-            name="prompt_tuning_embeddings",
-            model_type=embeddings_llm_settings.type,
-            config=embeddings_llm_settings,
-            callbacks=NoopWorkflowCallbacks(),
-            cache=None,
-        )
 
-        chunks_df, embeddings = await _embed_chunks(
-            batch_size,
-            batch_max_tokens,
-            splitter=splitter,
-            text_chunks=chunks_df,
-            embedding_llm=embedding_llm,
-            config=embeddings_llm_settings,
-            logger=logger,
-            n_subset_max=n_subset_max,
+        """Convert text chunks into dense text embeddings."""
+        sampled_text_chunks = chunks_df.sample(n=min(n_subset_max, len(chunks_df)))[
+            "text"
+        ].tolist()
+
+        embedding_results = await run_embed_text(
+            sampled_text_chunks,
+            callbacks=NoopWorkflowCallbacks(),
+            cache=NoopPipelineCache(),
+            args={
+                "llm": embeddings_llm_settings.model_dump(),
+                "num_threads": embeddings_llm_settings.concurrent_requests,
+                "batch_size": config.embed_text.batch_size,
+                "batch_max_tokens": config.embed_text.batch_max_tokens,
+            },
         )
+        embeddings = np.array(embedding_results.embeddings)
         chunks_df = _sample_chunks_from_embeddings(chunks_df, embeddings, k=k)
 
     # Convert the dataset to list form, so we have a list of documents
