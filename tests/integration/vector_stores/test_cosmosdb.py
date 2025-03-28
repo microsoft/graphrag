72fd984abe074c4897923149cf5dbf04ca438972
Kenny Zhang <kzhang678@gmail.com>
2025-03-28 15:39:40 -0400

    reset emulator connection string

--- tests/integration/vector_stores/test_cosmosdb.py
+++ tests/integration/vector_stores/test_cosmosdb.py
@@ -11,7 +11,7 @@ from graphrag.vector_stores.base import VectorStoreDocument
 from graphrag.vector_stores.cosmosdb import CosmosDBVectorStore
 
 # cspell:disable-next-line well-known-key
-WELL_KNOWN_COSMOS_CONNECTION_STRING = "AccountEndpoint=http://127.0.0.1:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+WELL_KNOWN_COSMOS_CONNECTION_STRING = "AccountEndpoint=https://127.0.0.1:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 
 # the cosmosdb emulator is only available on windows runners at this time
 if not sys.platform.startswith("win"):
