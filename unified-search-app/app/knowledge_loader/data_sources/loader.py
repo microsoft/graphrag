5b5c00fb932f89b23422ddf93b4cc5a5dcd51374
Gaudy Blanco <gaudy-microsoft@MacBook-Pro-m4-Gaudy-For-Work.local>
2025-04-03 23:44:29 -0600

    fix lint error

--- unified-search-app/app/knowledge_loader/data_sources/loader.py
+++ unified-search-app/app/knowledge_loader/data_sources/loader.py
@@ -32,7 +32,7 @@ def _get_base_path(
     dataset: str | None, root: str | None, extra_path: str | None = None
 ) -> str:
     """Construct and return the base path for the given dataset and extra path."""
-    return os.path.join( # noqa: PTH118
+    return os.path.join(  # noqa: PTH118
         os.path.dirname(os.path.realpath(__file__)),  # noqa: PTH120
         root if root else "",
         dataset if dataset else "",
