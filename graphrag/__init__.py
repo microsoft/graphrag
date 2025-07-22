1a4195c0ca058daa93f756d1728f3fd972263ee0
Josh Bradley <joshbradley@microsoft.com>
2025-07-22 15:20:24 -0400

    remove hardcoded version string

--- graphrag/__init__.py
+++ graphrag/__init__.py
@@ -9,6 +9,3 @@ from graphrag.logger.standard_logging import init_console_logger
 
 logger = logging.getLogger(__name__)
 init_console_logger()
-
-# Version is set in pyproject.toml
-__version__ = "2.4.0"
