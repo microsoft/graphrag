a77ecffc792bc6fc87770da6cdc540bff51341c8
Gaudy Blanco <gaudy-microsoft@MacBook-Pro-m4-Gaudy-For-Work.local>
2025-10-23 10:42:28 -0600

    feedback implemented

--- graphrag/config/embeddings.py
+++ graphrag/config/embeddings.py
@@ -37,5 +37,5 @@ def create_index_name(
         raise KeyError(msg)
 
     if index_prefix:
-        return f"{index_prefix}_{embedding_name}".replace(".", "-")
-    return embedding_name.replace(".", "_")
+        return f"{index_prefix}_{embedding_name}"
+    return embedding_name
