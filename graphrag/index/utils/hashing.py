e58f2ec7ddb7d4939eceb0fc153b20ae4a1acd32
q33566 (Kurt) <qaz57123@gmail.com>
2025-11-09 16:19:03 +0800

    chore(types): add return type hint to gen_sha512_hash

--- graphrag/index/utils/hashing.py
+++ graphrag/index/utils/hashing.py
@@ -8,7 +8,7 @@ from hashlib import sha512
 from typing import Any
 
 
-def gen_sha512_hash(item: dict[str, Any], hashcode: Iterable[str]):
+def gen_sha512_hash(item: dict[str, Any], hashcode: Iterable[str]) -> str:
     """Generate a SHA512 hash."""
     hashed = "".join([str(item[column]) for column in hashcode])
     return f"{sha512(hashed.encode('utf-8'), usedforsecurity=False).hexdigest()}"
