b2403c6fceb7e2fad5564efb453c067a1ed60ee6
Gaudy Blanco <gaudy-microsoft@MacBook-Pro-m4-Gaudy-For-Work.local>
2025-11-11 15:55:59 -0600

    remove optional embeddings

--- packages/graphrag/graphrag/config/embeddings.py
+++ packages/graphrag/graphrag/config/embeddings.py
@@ -3,22 +3,12 @@
 
 """A module containing embeddings values."""
 
-entity_title_embedding = "entity.title"
-entity_description_embedding = "entity.description"
-relationship_description_embedding = "relationship.description"
-document_text_embedding = "document.text"
-community_title_embedding = "community.title"
-community_summary_embedding = "community.summary"
-community_full_content_embedding = "community.full_content"
-text_unit_text_embedding = "text_unit.text"
+entity_description_embedding = "entity_description"
+community_full_content_embedding = "community_full_content"
+text_unit_text_embedding = "text_unit_text"
 
 all_embeddings: set[str] = {
-    entity_title_embedding,
     entity_description_embedding,
-    relationship_description_embedding,
-    document_text_embedding,
-    community_title_embedding,
-    community_summary_embedding,
     community_full_content_embedding,
     text_unit_text_embedding,
 }
