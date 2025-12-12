ef97d7bcba38015d4d0bef6d0d9546050297373c
Derek Worthen <worthend.derek@gmail.com>
2025-12-12 07:30:04 -0800

    cleanup

--- packages/graphrag/graphrag/storage/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024 Microsoft Corporation.
-# Licensed under the MIT License
-
-"""The storage package root."""
-
-from graphrag_storage import create_storage, register_storage
-
-__all__ = ["create_storage", "register_storage"]
